43d770a31eecfd2bfeda8094fe21d9d04682e34c
Abdihakim <160171553+Abdihakim-40@users.noreply.github.com>
2024-12-19 13:43:19 +0300

    updated_file

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+  
+ 
+  
+function updateEmployeeWithKeyAndValue (employee){
+    employee = {};
+    employee.name = 'Sam';
+    employee.streetAddress= "11 Broadway"
+
+    return employee
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee) {
+    employee['streetAddress'] ="12 Broadway";
+    return employee;
+
+    
+}
+
+
+function deleteFromEmployeeByKey(employee) {
+   
+    const newEmployee = {  name: 'joe', streetAddress: '12 Broadway' };
+    delete newEmployee['name']
+    return newEmployee
+    
+}
+
+// Destructive delete function
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee;  
+}
+
